9ff91a648c2df799b0528951fe3f283b666e5d2c
Xu-pixel <naomi1299@163.com>
2022-04-29 13:23:38 +0800

    把对话框搬到Dialog模块

---
+++ src/components/Dialog.vue
@@ -0,0 +1,34 @@
+<template>
+    <div  ref="postContainer">
+        <div v-for="(post, i) in posts" class="flex px-2 md:w-2/3 w-11/12"
+            :class="post.cardColor === bgColor && post.nickName === nickName ? ' justify-end' : 'justify-start'">
+            <div class="my-2 p-2 flex flex-col rounded-b-xl"
+                :class="post.cardColor === bgColor && post.nickName === nickName ? 'ml-4 items-end  rounded-l-xl' : 'mr-4 items-start rounded-r-xl'"
+                :style="{ backgroundColor: post.cardColor }">
+                <div class=" font-bold">{{ post.nickName }}</div>
+                <div class="py-1"> {{ post.text }}</div>
+                <div class="text-xs">{{ post.time }}</div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import { onUpdated,ref } from "vue";
+export default {
+    props: ['posts', 'bgColor', 'nickName'],
+    setup(props) {
+        onUpdated(() => {
+            scrollButtom();
+        });
+        const postContainer = ref(null);
+        const scrollButtom = () => {
+            postContainer.value.scrollTop = postContainer.value.scrollHeight - postContainer.value.clientHeight;
+        };
+
+        return {
+            postContainer
+        }
+    }
+}
+</script>
--- src/components/Talk.vue
+++ src/components/Talk.vue
@@ -11,17 +11,7 @@
             <p v-else style="color: red;">离线</p>
             <p>socketId:{{ socketId }}</p>
         </div>
-        <div class="w-full grow h-20 overflow-auto scroll-smooth flex flex-col items-center" ref="postContainer">
-            <div v-for="(post, i) in posts" class="flex px-2 sm:w-2/3 w-11/12" :class="post.cardColor === bgColor && post.nickName === nickName ? ' justify-end':'justify-start'">
-                <div class="my-2 p-2 flex flex-col rounded-b-xl"
-                    :class="post.cardColor === bgColor && post.nickName === nickName ? 'ml-4 items-end  rounded-l-xl':'mr-4 items-start rounded-r-xl'"
-                    :style="{ backgroundColor: post.cardColor }">
-                    <div class=" font-bold">{{ post.nickName }}</div>
-                    <div class="py-1"> {{ post.text }}</div>
-                    <div class="text-xs">{{ post.time }}</div>
-                </div>
-            </div>
-        </div>
+        <Dialog :nickName="nickName" :posts="posts" :bgColor="bgColor" class="w-full grow h-20 overflow-auto scroll-smooth flex flex-col items-center"></Dialog>
         <div class="flex w-full bg-white h-16 flex-none">
             <input @focus="scrollButtom"
                 @keyup.enter="sendPost"
@@ -34,70 +24,65 @@
 </template>
 
 <script>
-import { getCurrentInstance, onBeforeUpdate, onMounted, onUpdated, reactive, ref, toRefs, watch, watchEffect } from "vue"
+import { getCurrentInstance, reactive, ref, toRefs, watch, watchEffect } from "vue"
 import uniqolor from 'uniqolor';
+import Dialog from "./Dialog.vue";
 
 
 export default {
     setup() {
-        const { proxy } = getCurrentInstance()
+        const { proxy } = getCurrentInstance();
         const profile = reactive({
-            nickName: localStorage.getItem('nickName') || 'anonymity',
-            talkId: localStorage.getItem('uuid'),
-        })
-        let bgColor = ref(uniqolor(profile.talkId, { format: 'hsl', saturation: [45,55], lightness: [93,98] }).color)
+            nickName: localStorage.getItem("nickName") || "anonymity",
+            talkId: localStorage.getItem("uuid"),
+        });
+        let bgColor = ref(uniqolor(profile.talkId, { format: "hsl", saturation: [45, 55], lightness: [93, 98] }).color);
         const state = reactive({
-            socketId: '',
+            socketId: "",
             connected: false
-        })
-        const rooms = ref([])
-        const posts = ref([])
-        const inputBox = ref('')
-        const postContainer = ref(null)
-
-
+        });
+        const rooms = ref([]);
+        const posts = ref([]);
+        const inputBox = ref("");
+        const postContainer = ref(null);
         const changeNickName = () => {
-            const nickName = prompt('输入名字', profile.nickName)
-            profile.nickName = nickName
-        }
-
-        console.log(proxy.$socket)
-        proxy.$socket.on('message', message => console.log(message))
-        proxy.$socket.on('connect', () => {
-            state.connected = proxy.$socket.connected
-            state.socketId = proxy.$socket.id
-        })
-        proxy.$socket.on('disconnect', () => {
-            state.connected = proxy.$socket.connected
-            state.socketId = proxy.$socket.id
-        })
-
+            const nickName = prompt("输入名字", profile.nickName);
+            profile.nickName = nickName;
+        };
+        console.log(proxy.$socket);
+        proxy.$socket.on("message", message => console.log(message));
+        proxy.$socket.on("connect", () => {
+            state.connected = proxy.$socket.connected;
+            state.socketId = proxy.$socket.id;
+        });
+        proxy.$socket.on("disconnect", () => {
+            state.connected = proxy.$socket.connected;
+            state.socketId = proxy.$socket.id;
+        });
         //login
-        proxy.$socket.emit('login', profile.talkId)
-        proxy.$socket.on('login', (uuid, exist) => {
+        proxy.$socket.emit("login", profile.talkId);
+        proxy.$socket.on("login", (uuid, exist) => {
             if (exist) {
-                console.log('已经登录')
-            } else {
-                profile.talkId = uuid
+                console.log("已经登录");
             }
-        })
-
+            else {
+                profile.talkId = uuid;
+            }
+        });
         //logout
         const logout = () => {
-            proxy.$socket.emit('logout', profile.talkId)
-            profile.talkId = ''
-        }
-
+            proxy.$socket.emit("logout", profile.talkId);
+            profile.talkId = "";
+        };
         //post
-        proxy.$socket.on('post', (cardColor, nickName, text, time) => {
+        proxy.$socket.on("post", (cardColor, nickName, text, time) => {
             posts.value.push({
                 cardColor: cardColor,
                 nickName: nickName,
                 text: text,
                 time: time,
-            })
-        })
-
+            });
+        });
         //functions
         const sendPost = () => {
             posts.value.push({
@@ -105,47 +90,27 @@ export default {
                 nickName: profile.nickName,
                 text: inputBox.value,
                 time: Date()
-            })
-            proxy.$socket.emit('post', bgColor.value, profile.nickName, inputBox.value, Date())
-            inputBox.value = ''
+            });
+            proxy.$socket.emit("post", bgColor.value, profile.nickName, inputBox.value, Date());
+            inputBox.value = "";
             // postContainer.value.scrollTop = postContainer.value.scrollHeight - postContainer.value.clientHeight
             // console.log(postContainer.value.scrollTop)
-        }
-
+        };
         const reload = () => {
-            location.reload()
-        }
-
-        const scrollButtom = () => {
-            postContainer.value.scrollTop = postContainer.value.scrollHeight - postContainer.value.clientHeight
-        }
-
+            location.reload();
+        };
 
         //监视
-        watch(
-            () => profile.nickName,
-            (nickName) => localStorage.setItem('nickName', nickName)
-        )
-
-        watch(
-            () => profile.talkId,
-            (talkId) => {
-                localStorage.setItem('uuid', talkId)
-                console.log(talkId)
-            }
-        )
-
-        onUpdated(() => {
-            scrollButtom()
-        })
-
+        watch(() => profile.nickName, (nickName) => localStorage.setItem("nickName", nickName));
+        watch(() => profile.talkId, (talkId) => {
+            localStorage.setItem("uuid", talkId);
+            console.log(talkId);
+        });
 
         watchEffect(() => {
-            console.log(state.connected)
-            console.log(state.socketId)
-
-        })
-
+            console.log(state.connected);
+            console.log(state.socketId);
+        });
         return {
             ...toRefs(profile),
             ...toRefs(state),
@@ -153,13 +118,13 @@ export default {
             logout,
             reload,
             sendPost,
-            scrollButtom,
             inputBox,
             posts,
             postContainer,
             bgColor
-        }
-    }
+        };
+    },
+    components: { Dialog }
 }
 </script>
 
